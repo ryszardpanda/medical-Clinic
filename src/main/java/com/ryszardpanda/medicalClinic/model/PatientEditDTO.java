90333be3418732710c3c2554dce221e739dffc3b
Ryszard Różański <rychu48@op.pl>
2024-11-10 21:04:02 +0100

    MED-011 + Added PatientEditDTO + Small logic changes in repository and service

---
+++ src/main/java/com/ryszardpanda/medicalClinic/model/PatientEditDTO.java
@@ -0,0 +1,28 @@
+package com.ryszardpanda.medicalClinic.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Data
+public class PatientEditDTO {
+    @NotBlank(message = "Imie nie może być puste, bądź być nullem")
+    private String firstName;
+    @NotBlank(message = "Nazwisko nie może być puste, bądź być nullem")
+    private String lastName;
+    @Email(message = "Zły format email")
+    @NotBlank(message = "Email nie może być puste, bądź być nullem")
+    private String email;
+    @NotBlank(message = "Password nie może być puste, bądź być nullem")
+    private String password;
+    private String idCardNo;
+    @NotBlank(message = "Telefon nie może być pusty, bądź być nullem")
+    private String phoneNumber;
+    @NotNull(message = "Data nie może być pusta, bądź być nullem")
+    private LocalDate birthday;
+}
